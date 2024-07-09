28c7ef55181ed4fef0118381ac2a180fa95b9843
Rene <canedarene@gmail.com>
2024-07-08 18:39:03 -0600

    cambio y implementacion del menu a utilizar

---
+++ src/ConvertidorM.java
@@ -0,0 +1,13 @@
+public class ConvertidorM {
+    public static void main(String[] args) {
+//menu
+        //menu
+        String menu = """
+               ***Selcciona la moneda que quieras hacer la conversion***
+               1.- Consultar saldo 
+               2.- Retirar
+               3.- Depositar
+               4.- Salir
+               """;
+    }
+}
